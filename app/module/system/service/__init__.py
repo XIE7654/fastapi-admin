ea3675bca2fb777bc1985ec4ac35ba9b11b8a3f2
xie7654 <765462425@qq.com>
2026-03-23 12:04:24 +0800

    1

--- app/module/system/service/__init__.py
+++ app/module/system/service/__init__.py
@@ -8,7 +8,7 @@ from app.module.system.service.menu import MenuService
 from app.module.system.service.dept import DeptService
 from app.module.system.service.post import PostService
 from app.module.system.service.dict import DictService
-from app.module.system.service.operate_log import OperateLogService, log_operation
+from app.module.system.service.operate_log import OperateLogService
 from app.module.system.service.login_log import LoginLogService
 from app.module.system.service.config import ConfigService
 from app.module.system.service.online_user import OnlineUserService
@@ -25,7 +25,6 @@ __all__ = [
     "PostService",
     "DictService",
     "OperateLogService",
-    "log_operation",
     "LoginLogService",
     "ConfigService",
     "OnlineUserService",
